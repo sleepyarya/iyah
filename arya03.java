868cb2d47da8e0a7de3178e78f2f8467fb4fb375
sleepyarya <jjill2479@gmail.com>
2024-09-05 15:30:44 +0700

    sip

--- arya03.java
+++ arya03.java
@@ -1,6 +1,6 @@
 public class arya03 {
 
     public static void main(String[] args) {
-        System.out.println("Saya Arya Ramadhan");
+        System.out.println("Saya Arya Ramadhan jir");
     }
 }
